600a7379511773d042fa38530a700c51256bb6d0
killinsun <noneck.0311@gmail.com>
2022-03-12 17:22:05 +0900

    handle chain

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"reflect"
+	"runtime"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello! HandlerFunc~")
 }
 
-func world(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "World! HandlerFunc~")
+func log(h http.HandlerFunc) http.HandlerFunc { 
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called : " + name)
+		h(w, r)
+	}
 }
 
 func main () {
@@ -21,8 +27,7 @@ func main () {
 
 	}
 
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/world", world)
+	http.HandleFunc("/hello", log(hello))
 
 	server.ListenAndServe()
 }
